05346e17beba18ab1e07dcc8d9c8b7e8083a3540
Mário Brandão <mariorochedo329@gmail.com>
2024-10-28 21:43:06 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,7 +1,5 @@
 import os
-from time import sleep
 import pandas as pd
-import numpy as np
 from openpyxl.styles import PatternFill
 
 def registrar_nota():
@@ -9,80 +7,80 @@ def registrar_nota():
     nome = input('Digite o nome do aluno: ')
     alunos = {}
     notas = []
-    media = 0
-    while True:
+    for n in range(1, 5):
         nota = float(input('Digite a nota do aluno[-1 --> Sair]: '))
-        if nota < 0:
-            break
-        media += nota
-    notas.append(media)
+        notas.append(nota)
+    
+    media = sum(notas) / len(notas)
 
     alunos['nome'] = nome
-    alunos['notas'] = notas
+    alunos['notas'] = media
     print(alunos)
-    return alunos
+    return {'Nome': nome, 'Nota': f'{media:.2f}'}
 
 def media(alunos):
     #Colocar notas em uma lista para colocar na tabela excel
     os.system('clear')
     alunos_aprovados = []
+    
     alunos_recuperacao = []
     alunos_reprovados = []
     for aluno in alunos:
-        for nota in aluno['notas']:
-            if 3 < nota < 6:
-                alunos_recuperacao.append([aluno['nome'], nota]) #Adicionei listas ordenadas aqui
+        if 5 < aluno['Nota'] < 7:
+            alunos_recuperacao.append(aluno)
+        elif aluno['Nota'] >= 7:
+            alunos_aprovados.append(aluno)
+        else:
+            alunos_reprovados.append(aluno)
                 
-                
-            elif nota >= 7:
-                alunos_aprovados.append([aluno['nome'], nota]) #Adicionei listas ordenadas aqui
-        
-        
-            else:
-                alunos_reprovados.append([aluno['nome'], nota]) #Adicionei listas ordenadas aqui
-                        
-    print(alunos_aprovados)
-    print(alunos_recuperacao)
-    print(alunos_reprovados)
-    exportar_excel(aprovados=alunos_aprovados, recuperacao=alunos_recuperacao ,reprovados=alunos_reprovados)
+    return alunos_aprovados, alunos_recuperacao, alunos_reprovados
+
 
 def atualizar_nota():
     #Desenvolver isso aqui
     pass
 
 def exportar_excel(aprovados, recuperacao, reprovados):
-    #Ja coloquei as listas e notas ordenadas, agora é fazer as tabelas no excel
+    #colocar as notas inves das situações
     dados_aprovados = {
-        'Nome': aprovados,
-        'Situação': ['Aprovado'] * len(aprovados)
+        'Nome': [aluno['Nome'] for aluno in aprovados],
+        'Nota': [aluno['Nota'] for aluno in aprovados],
+        'Situação': 'Aprovado'
     }
     dados_recuperacao = {
-        'Nome': recuperacao,
-        'Situação': 'Recuperação' * len(recuperacao)
+        'Nome': [aluno['Nome'] for aluno in recuperacao],
+        'Nota': [aluno['Nota'] for aluno in recuperacao],
+        'Situação': 'Recuperação'
     }
     dados_reprovados = {
-        'Nome': reprovados,
-        'Situação': 'Reprovados' * len(reprovados)
+        'Nome': [aluno['Nome'] for aluno in reprovados],
+        'Nota': [aluno['Nota'] for aluno in reprovados],
+        'Situação': 'Reprovado'
     }
-
-    dataframe = pd.DataFrame(dados_aprovados)
+    
+    dataframeApr = pd.DataFrame(dados_aprovados)
+    dataframeRec = pd.DataFrame(dados_recuperacao)
+    dataframeRpv = pd.DataFrame(dados_reprovados)
 
     with pd.ExcelWriter('Alunos.xlsx', engine='openpyxl') as writer:
-        dataframe.to_excel(writer, sheet_name='Alunos', index=False)
-
+        dataframeApr.to_excel(writer, sheet_name='Aprovados', index=False)
+        dataframeRec.to_excel(writer, sheet_name='Recuperação', index=False)
+        dataframeRpv.to_excel(writer, sheet_name='Reprovados', index=False)
+        
         workbook = writer.book
-        worksheet = writer.sheets['Alunos']
+        worksheetApr = writer.sheets['Aprovados']
+        worksheetRec = writer.sheets['Recuperação']
+        worksheetRpv = writer.sheets['Reprovados']
 
         verde = PatternFill(start_color='00FF00', end_color='00FF00', fill_type='solid')
         amarelo = PatternFill(start_color='FFFF00', end_color='FFFF00', fill_type='solid')
         vermelho = PatternFill(start_color='FF0000', end_color='FF0000', fill_type='solid')
 
-        for row in range(2, len(dataframe) + 2):
-            status = worksheet.cell(row=row, column=2).value
-            if status == 'Aprovado':
-                worksheet.cell(row=row, column=2).fill = verde
-            elif status == 'Recuperação':
-                worksheet.cell(row=row, column=2).fill = amarelo
-            elif status == 'Reprovado':
-                worksheet.cell(row=row, column=2).fill = vermelho
-    print(dataframe)    
+        for row in range(2, len(dataframeApr) + 2):
+            worksheetApr.cell(row=row, column=3).fill = verde
+
+        for row in range(2, len(dataframeRec) + 2):
+            worksheetRec.cell(row=row, column=3).fill = amarelo    
+    
+        for row in range(2, len(dataframeRec) + 2):
+            worksheetRpv.cell(row=row, column=3).fill = vermelho
